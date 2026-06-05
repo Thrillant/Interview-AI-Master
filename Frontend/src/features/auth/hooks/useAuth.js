87abd80d2921c54060e540f3af0d7cb3b206973a
Thrillant <suvodip700060@gmail.com>
2026-06-05 22:03:48 +0530

    Updating UI, Hooks, AI & States

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,53 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    const {user, setUser, loading, setLoading} = context;
+    
+    const handleRegister = async ({username, email, password}) => {
+        setLoading(true);
+        try {
+            const data = await register({username, email, password});
+            setUser(data.user);
+        } catch(err) {
+
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const handleLogin = async ({email, password}) => {
+        setLoading(true);
+        try {
+            const data = await login({email, password});
+            setUser(data.user);
+        } catch(err) {
+
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const handleLogout = async () => {
+        setLoading(true);
+        try {
+            const data = await logout();
+            setUser(null);
+        } catch(err) {
+
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // const handleGetMe = async () => {
+    //     setLoading(true);
+    //     const data = await getMe();
+    //     setUser(data.user);
+    //     setLoading(false);
+    // }
+
+    return {user, loading, handleRegister, handleLogin, handleLogout};
+}
