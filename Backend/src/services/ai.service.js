5c586d449db6be793d16c8cae97bea957740992c
Thrillant <suvodip700060@gmail.com>
2026-06-12 20:43:06 +0530

    Fixed some files

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -105,7 +105,10 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 }
 
 async function convertHtmlToPdf(htmlContent) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: "new",
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
     const pdf = await page.pdf({
